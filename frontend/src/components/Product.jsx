024e8814b64cb365404f9c87ee85ca88b55db215
Rejay Sobi <rejaysobi@192.168.0.119>
2023-11-29 17:02:58 +0530

    modified

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -16,7 +16,7 @@ const Product = ({ product }) => {
         </Link>
 
         <Card.Text as='div'>
-          <Rating value={product.rating} text={`${product.numReviews}`} />
+          <Rating value={product.rating} text={` ${product.numReviews} reviews`} />
         </Card.Text>
 
         <Card.Text as='h3'>${product.price}</Card.Text>
