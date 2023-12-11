66ac8564b88efb422acc0072d42d9750b8009cab
Rejay Sobi <rejaysobi@Rejays-MacBook-Air.local>
2023-12-11 07:54:38 +0530

    Modified Readme file

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,17 +1,22 @@
 
 import { Row, Col } from 'react-bootstrap';
 import {useParams} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
+import Paginate from '../components/Paginate';
 
 
 const HomeScreen =()=>{
-  const {pageNumber}=useParams();
-  const{data,isLoading,error}=useGetProductsQuery({pageNumber});
+  const {pageNumber,keyword}=useParams();
+  const{data,isLoading,error}=useGetProductsQuery({keyword,pageNumber});
   return (
     <>
+    {keyword && <Link to='/' className='btn btn-light mb-4'>
+      Go Back
+      </Link>}
     {isLoading ? (
       <Loader/>
     ) : error ?(
@@ -27,6 +32,11 @@ const HomeScreen =()=>{
       </Col>
     ))}
   </Row>
+  <Paginate 
+  pages={data.pages} 
+  page={data.page} 
+  keyword={keyword ? keyword :''} 
+  />
 </>)}
 </>
       );
--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -117,17 +117,21 @@
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button, Row, Col } from 'react-bootstrap';
 import { FaEdit,FaTrash } from 'react-icons/fa';
-// import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
+import Paginate from '../../components/Paginate';
 import {toast} from 'react-toastify'
 import { useGetProductsQuery,
     useCreateProductMutation,
     useDeleteProductMutation } from '../../slices/productsApiSlice';
 
 
+    
+
 const ProductListScreen = () => {
-    const {data:products,isLoading,error,refetch}=useGetProductsQuery();
+    const {pageNumber}=useParams();
+    const {data,isLoading,error,refetch}=useGetProductsQuery({pageNumber});
     // console.log(products);
 
     const [createProduct,{isLoading:loadingCreate}]=useCreateProductMutation();
@@ -185,7 +189,7 @@ return (
                 </tr>
             </thead>
             <tbody>
-                {products.map((product)=>(
+                {data.products.map((product)=>(
                     <tr key={product._id}>
                         <td>{product._id}</td>
                         <td>{product.name}</td>
@@ -208,6 +212,7 @@ return (
                 ))}
             </tbody>
         </Table>
+        <Paginate pages={data.pages} page={data.page} isAdmin={true} />
         </>
     )}
     </>
